d70a9ef7499c4793a1e2a26da1d73a156c86c404
Clevsson <48739256+Clevsson@users.noreply.github.com>
2024-03-22 17:36:34 -0300

    .

--- app.js
+++ app.js
@@ -92,97 +92,161 @@
 ];*/
 
 
+
 const perguntas = [
     {
-        pergunta: "Qual é o principal objetivo do aconselhamento em psicologia?",
+        pergunta: "Qual figura histórica é frequentemente considerada como o fundador do behaviorismo?",
         respostas: [
-            "A) Diagnóstico de doenças mentais",
-            "B) Promover o autoconhecimento e o desenvolvimento pessoal",
-            "C) Prescrição de medicamentos psicotrópicos",
-        ],
-        correta: 1
-    },
-    {
-        pergunta: "Qual abordagem terapêutica se concentra na identificação e modificação de pensamentos negativos?",
-        respostas: [
-            "A) Psicanálise",
-            "B) Terapia Cognitivo-Comportamental",
-            "C) Gestalt-terapia",
-        ],
-        correta: 1
-    },
-    {
-        pergunta: "O que é empatia no contexto do aconselhamento psicológico?",
-        respostas: [
-            "A) Desinteresse pelas emoções do cliente",
-            "B) Compreensão e compartilhamento das emoções do cliente",
-            "C) Manipulação das emoções do cliente",
-        ],
-        correta: 1
-    },
-    {
-        pergunta: "Quais são as fases típicas de um processo de aconselhamento?",
-        respostas: [
-            "A) Introdução, conclusão e feedback",
-            "B) Exploração, compreensão e ação",
-            "C) Avaliação, diagnóstico e prescrição",
+            "A. Sigmund Freud",
+            "B. Ivan Pavlov",
+            "C. John B. Watson",
         ],
         correta: 2
     },
     {
-        pergunta: "O que é a transferência na psicanálise?",
+        pergunta: "Qual dos seguintes não é um princípio chave do behaviorismo?",
         respostas: [
-            "A) Troca de informações entre cliente e terapeuta",
-            "B) Projeção de sentimentos do cliente sobre o terapeuta",
-            "C) Interação social fora do consultório",
-        ],
-        correta: 1
-    },
-    {
-        pergunta: "Qual é a importância do rapport no aconselhamento psicológico?",
-        respostas: [
-            "A) Criar uma atmosfera de confiança e conexão",
-            "B) Diagnosticar rapidamente os problemas do cliente",
-            "C) Impor autoridade ao terapeuta",
+            "A. O foco no estudo da mente inconsciente",
+            "B. A importância do comportamento observável",
+            "C. A influência do ambiente no comportamento",
         ],
         correta: 0
     },
     {
-        pergunta: "O que significa a sigla HIPAA no contexto da privacidade em saúde nos Estados Unidos?",
+        pergunta: "Qual é a técnica usada pelo behaviorismo para estudar o comportamento animal?",
         respostas: [
-            "A) Health Information Privacy and Accountability Act",
-            "B) Human Insight and Personalized Analysis Act",
-            "C) Helping Individuals with Psychological Assistance and Awareness",
+            "A. Teoria da mente",
+            "B. Experimentação controlada",
+            "C. Introspecção",
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "O que o behaviorismo considera como a principal influência no comportamento humano?",
+        respostas: [
+            "A. Instinto inato",
+            "B. Experiências passadas",
+            "C. Genética",
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "Quem desenvolveu a teoria do condicionamento operante?",
+        respostas: [
+            "A. B.F. Skinner",
+            "B. Albert Bandura",
+            "C. Abraham Maslow",
         ],
         correta: 0
     },
     {
-        pergunta: "Qual é o papel do consentimento informado no aconselhamento psicológico?",
+        pergunta: "Qual é o objetivo principal do behaviorismo?",
         respostas: [
-            "A) Documentar as sessões de terapia",
-            "B) Obter permissão ética do cliente antes do tratamento",
-            "C) Diagnosticar doenças mentais",
+            "A. Compreender a mente inconsciente",
+            "B. Prever e controlar o comportamento",
+            "C. Explorar a autoconsciência",
         ],
         correta: 1
     },
     {
-        pergunta: "O que caracteriza a abordagem humanista no aconselhamento?",
+        pergunta: "O que é reforço positivo no contexto do behaviorismo?",
         respostas: [
-            "A) Foco na modificação de comportamentos",
-            "B) Valorização da autoatualização e crescimento pessoal",
-            "C) Ênfase na análise do inconsciente",
+            "A. Adicionar um estímulo para aumentar a frequência de um comportamento",
+            "B. Remover um estímulo para diminuir a frequência de um comportamento",
+            "C. Adicionar um estímulo para diminuir a frequência de um comportamento",
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Qual é a diferença entre condicionamento clássico e condicionamento operante?",
+        respostas: [
+            "A. Condicionamento clássico envolve associações entre estímulos, enquanto o condicionamento operante envolve associações entre comportamentos e consequências.",
+            "B. Condicionamento clássico envolve reforços positivos, enquanto o condicionamento operante envolve reforços negativos.",
+            "C. Condicionamento clássico é um processo consciente, enquanto o condicionamento operante é um processo inconsciente.",
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Qual é o papel do ambiente no behaviorismo?",
+        respostas: [
+            "A. O ambiente tem pouca influência sobre o comportamento humano.",
+            "B. O ambiente molda completamente o comportamento humano.",
+            "C. O ambiente interage com fatores genéticos para influenciar o comportamento humano.",
+        ],
+        correta: 2
+    },
+    {
+        pergunta: "Quem desenvolveu a teoria do condicionamento clássico?",
+        respostas: [
+            "A. John B. Watson",
+            "B. B.F. Skinner",
+            "C. Ivan Pavlov",
+        ],
+        correta: 2
+    },
+    {
+        pergunta: "Qual é o principal objetivo do behaviorismo aplicado?",
+        respostas: [
+            "A. Investigar processos mentais complexos",
+            "B. Aplicar os princípios do behaviorismo para resolver problemas práticos",
+            "C. Estudar a autoconsciência",
         ],
         correta: 1
     },
     {
-        pergunta: "Quais são as habilidades essenciais de um bom terapeuta?",
+        pergunta: "Como o behaviorismo aborda a aprendizagem?",
         respostas: [
-            "A) Diagnóstico preciso e prescrição de medicamentos",
-            "B) Empatia, escuta ativa e habilidades de comunicação",
-            "C) Autoridade e controle sobre o cliente",
+            "A. Através do estudo da mente inconsciente",
+            "B. Através do estudo do comportamento observável e de suas relações com o ambiente",
+            "C. Através da introspecção",
         ],
         correta: 1
     },
+    {
+        pergunta: "O que é punição no contexto do behaviorismo?",
+        respostas: [
+            "A. Adicionar um estímulo para aumentar a frequência de um comportamento",
+            "B. Remover um estímulo para diminuir a frequência de um comportamento",
+            "C. Adicionar um estímulo para diminuir a frequência de um comportamento",
+        ],
+        correta: 2
+    },
+    {
+        pergunta: "Qual é a contribuição de Edward Thorndike para o behaviorismo?",
+        respostas: [
+            "A. Desenvolvimento da teoria do condicionamento operante",
+            "B. Desenvolvimento da teoria do condicionamento clássico",
+            "C. Desenvolvimento da teoria da autoconsciência",
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "O que é condicionamento no behaviorismo?",
+        respostas: [
+            "A. Um processo de aprendizagem que envolve a associação entre estímulos",
+            "B. Um processo de aprendizagem que envolve a introspecção",
+            "C. Um processo de aprendizagem que envolve a autoconsciência",
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Qual é o termo usado para descrever a diminuição na frequência de um comportamento como resultado da falta de reforço?",
+        respostas: [
+            "A. Extinção",
+            "B. Punição",
+            "C. Generalização",
+        ],
+        correta: 0
+    },
+    {
+        pergunta: "Qual dos seguintes é um exemplo de reforço negativo?",
+        respostas: [
+            "A. Receber um elogio por bom comportamento",
+            "B. Evitar uma multa por dirigir abaixo do limite de velocidade",
+            "C. Receber uma recompensa por completar uma tarefa",
+        ],
+        correta: 1
+    }
 ];
 
 
